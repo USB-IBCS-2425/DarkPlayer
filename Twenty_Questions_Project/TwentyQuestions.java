ea0419de29711d446282068cc6673796d3ba9f7f
Boen Liu <boen19906@gmail.com>
2024-10-09 13:14:28 -0700

    Twenty Questions updated

--- Twenty_Questions_Project/TwentyQuestions.java
+++ Twenty_Questions_Project/TwentyQuestions.java
@@ -23,27 +23,37 @@ class TwentyQuestions {
         questions = 0;
     }
 
+    public void guess() {
+        if (cur_pos.size() == 0) {
+            ans = "Described team doesn't exist";
+        } 
+        else if (cur_pos.size() > 1) {
+            ans = "Sorry I couldn't figure it out.";
+        }
+        else {
+            ans = cur_pos.get(0);
+        }
+
+        System.out.println("Your team is the " + ans);
+    }
     public void askQuestion() {
     	String[] queses = {"Is the team in the NFC?", "Is the team in the North or South divisions of their conference?",
     			"Has the team won a Super Bowl?", "Does the team have warm colors (red or orange) as their primary color on any of their jerseys?",
-    			"Is the team name an animal?", "Is the team's home stadium in the Western US (relative to Mississipi River)"
+    			"Is the team name an animal?", "Is the team's home stadium in the Western US (relative to Mississipi River)", "Did the team make the playoffs in 2023"
 		};
         
         Scanner s = new Scanner(System.in);
         
 
          System.out.println("Answer the following the questions with yes or no.");
-        while (cur_pos.size() > 1 && questions < 6) {
+        while (cur_pos.size() > 1 && questions < 7) {
         	System.out.println(queses[questions]);
         	String res = s.nextLine();
         	updateAnswers((res.equals("yes")), queses[questions]);
         	questions++;
+            
         }
-        if (cur_pos.size() == 0) {
-        	ans = "Described team doesn't exist";
-        } else {
-        	ans = cur_pos.get(0);
-        }
+        
         
         
 
@@ -105,15 +115,17 @@ class TwentyQuestions {
         //Question 4
         else if (ques.equals("Does the team have warm colors (red or orange) as their primary color on any of their jerseys?")) {
         	if (res) {
-        		cur_pos.removeAll(Arrays.asList("Pittsburgh Steelers", "Minnesota Vikings", "Green Bay Packers","Atlanta Falcons", "Dallas Cowboys", 
-	                "Philadelphia Eagles", "Detroit Lions", "Carolina Panthers", "Seattle Seahawks", "Los Angeles Rams", "Baltimore Ravens", "Miami Dolphins", 
-	                "Tennessee Titans", "Indianapolis Colts", "Los Angeles Chargers", "Jacksonville Jaguars"
+        		cur_pos.removeAll(Arrays.asList("Minnesota Vikings",  "Dallas Cowboys", 
+	                "Philadelphia Eagles", "Detroit Lions", "Carolina Panthers", "Seattle Seahawks", "Baltimore Ravens", 
+	               "Indianapolis Colts", "Jacksonville Jaguars", "Las Vegas Raiders", "New Orleans Saints", "Los Angeles Rams"
+
         		));
         	} else {
         		cur_pos.removeAll(Arrays.asList("Chicago Bears", "Arizona Cardinals", "San Francisco 49ers", "Kansas City Chiefs", 
 	        		"Tampa Bay Buccaneers", "Buffalo Bills", "New England Patriots", "Houston Texans", "Washington Commanders", 
-	        		"Cincinnati Bengals", "Cleveland Browns", "Denver Broncos", "New York Giants", "Las Vegas Raiders"
-        		));
+	        		"Cincinnati Bengals", "Cleveland Browns", "Denver Broncos", "New York Giants","Atlanta Falcons","Pittsburgh Steelers","Green Bay Packers",
+                    "Miami Dolphins", "Tennessee Titans", "Los Angeles Chargers"    		
+                ));
         	}
         	
         }
@@ -121,6 +133,7 @@ class TwentyQuestions {
         //Question 5
         else if (ques.equals("Is the team name an animal?")) {
         	if (res) {
+
         		cur_pos.removeAll(Arrays.asList("Dallas Cowboys", "New York Giants", "San Francisco 49ers", "Kansas City Chiefs", "Tampa Bay Buccaneers", 
         			"New England Patriots", "Washington Commanders", "Cleveland Browns", "Green Bay Packers", "Minnesota Vikings", "Pittsburgh Steelers", 
         			"Indianapolis Colts", "Las Vegas Raiders", "Los Angeles Chargers","Tennessee Titans", "Houston Texans", "Buffalo Bills"
@@ -148,6 +161,22 @@ class TwentyQuestions {
         		));
         	}
         }
+        //Question 7 (make everything unique)
+        else if (ques.equals("Did the team make the playoffs in 2023")) {
+
+            if (res) {
+                System.out.println("hello");
+                cur_pos.removeAll(Arrays.asList("Arizona Cardinals", "Atlanta Falcons", "Carolina Panthers", "Chicago Bears", "Cleveland Browns", "Denver Broncos", "Detroit Lions", 
+                    "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Las Vegas Raiders", "Seattle Seahawks", "New England Patriots", "New Orleans Saints", "New York Jets", 
+                    "Pittsburgh Steelers", "Tennessee Titans", "Washington Commanders"
+                ));
+            } else {
+                cur_pos.removeAll(Arrays.asList("Kansas City Chiefs", "Buffalo Bills", "Cincinnati Bengals", "Jacksonville Jaguars", "Baltimore Ravens", "Miami Dolphins", 
+                    "Los Angeles Chargers", "Philadelphia Eagles", "San Francisco 49ers", "Dallas Cowboys", "Minnesota Vikings", "Tampa Bay Buccaneers", "New York Giants", 
+                    "Los Angeles Rams"
+                ));
+            }
+        }
 
 
 
@@ -157,6 +186,8 @@ class TwentyQuestions {
     public static void main(String[] args) {
         TwentyQuestions game = new TwentyQuestions();
         game.askQuestion();
-        System.out.println(game.ans);
+        game.guess();
+
+        
     }
 }
