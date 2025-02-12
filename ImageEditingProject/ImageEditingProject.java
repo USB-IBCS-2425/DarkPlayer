078fc63b06677aac21a66784e6eba87a86cf7c6b
Boen Liu <boen19906@gmail.com>
2025-02-12 10:43:42 -0800

    citing sources

--- ImageEditingProject/ImageEditingProject.java
+++ ImageEditingProject/ImageEditingProject.java
@@ -162,6 +162,7 @@ class ImageEditingProject {
                             im.setRGB(i, j, col);
                         } else {
                             int gray = (int)(0.299 * r + 0.587 * g + 0.114 * b);
+                            //this is a formula i got from https://support.ptc.com/help/mathcad/r10.0/en/index.html#page/PTC_Mathcad_Help/example_grayscale_and_color_in_images.html
                             int col = (0xFF << 24) | (gray << 16) | (gray << 8) | gray;
                             im.setRGB(i, j, col);
                         }
